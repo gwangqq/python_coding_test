19667f0d910515773712894b947149b2cc9ec5f8
gwanggyu park <pgg961@gmail.com>
2021-02-09 21:12:50 +0900

    grredy 2

--- greedy_2.py
+++ greedy_2.py
@@ -1,5 +1,6 @@
-str = "JEROEN"
-
+# str = "JEROEN"
+# str = "JAN"
+str = "JAZ"
 
 cnt = 0
 # tmp = ord(x) - 65
@@ -7,16 +8,26 @@ cnt = 0
 # J = 74 -> 74-65 +1
 # M = 77 -> 77-65 +1
 # Z = 90 -> 91-90
-
+cnt_a = 0
+length = len(str)
 for x in str:
     # print(ord(x))
     tmp = ord(x) - 65
     print(tmp)
+
     if tmp <= 12:
         cnt += tmp
         print(cnt)
+        if tmp == 0:
+            cnt_a
+            print(cnt_a)
     elif tmp >= 13:
-        cnt += (26-tmp)
+        cnt += (26 - tmp)
         print(cnt)
 
-print(cnt + (len(str) - 2))
+if cnt_a >= 1:
+    answer = cnt + (length - 2)
+else:
+    answer = cnt + length - 1
+
+print(answer)
